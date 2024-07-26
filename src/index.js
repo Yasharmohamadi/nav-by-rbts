c05f1d136ab4217e8d92765ddf67856030e580ca
Yashar <yashar22mo@gmail.com>
2024-07-26 16:35:29 +0330

    Add Git to Dir

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+// FIRST WAY TO USE REACT
+// ReactDOM.render(
+//     <Main />,
+//     document.getElementById('root')
+// );
+
+// SECOND WAY TO USE REACT
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+);
